131fa1371ad524cabd4b0a2ae7e93dceb6d9c12f
Mr. Lamont <adam_lamont@wrdsb.ca>
2019-02-27 09:21:02 -0500

    Added conditions example

--- src/Examples/Recursion.java
+++ src/Examples/Recursion.java
@@ -39,12 +39,76 @@ public class Recursion {
     }
     
     
+    public static int gcd(int a, int b){
+        // base case
+        if(b == 0){
+            return a;
+        }
+        // do Euclidean Algorithm
+        return gcd(b, a%b);
+    }
+    
+    
+    public static int fact(int n){
+        // base case
+        if(n == 1 || n == 0){
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+    
+    
+    public static int pies(int[] sugar, int sugarEaten){
+        // base case, no pies, done eating
+        if(sugar.length == 0){
+            // nothing to eat
+            return sugarEaten;
+        }else if (sugar.length == 1){
+            // just eat the last one
+            return sugarEaten + sugar[0];
+        }
+        
+        // try eating the pie
+        int newSugarEaten = sugarEaten + sugar[0];
+        // remove appropriate pies
+        int[] newPies = new int[sugar.length - 2];
+        // copy old pies into new
+        for(int i = 2; i < sugar.length; i++){
+            newPies[i-2] = sugar[i];
+        }
+        // move to next pies
+        int pieEaten = pies(newPies, newSugarEaten);
+        
+        // try don't eat the pie
+        // no new sugar for me
+        newSugarEaten = sugarEaten;
+        // get this pie away from me
+        newPies = new int[sugar.length - 1];
+        // copy old pies
+        for(int i = 1; i < sugar.length; i++){
+            newPies[i-1] = sugar[i];
+        }
+        // move on
+        int noPieEaten = pies(newPies, newSugarEaten);
+        
+        // return the biggest one
+        return Math.max(pieEaten, noPieEaten);
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int fib5 = fibLoop(45);
-        System.out.println("F45 = " + fib5);
+       // int fib5 = fibLoop(45);
+       // System.out.println("F45 = " + fib5);
+       
+       //int gcd = gcd(36,8);
+        //System.out.println(gcd);
+       
+        int[] pies = {12,15,2,10,11,10};
+        int amount = pies(pies, 0);
+        System.out.println(amount);
+        
     }
     
 }
